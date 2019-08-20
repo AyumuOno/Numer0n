3fe051b1fc6a5987421a72152c76301e6ab6ba7b
小野歩 <onoayumu@onoayuminoMacBook-Pro.local>
2019-08-20 12:47:47 +0900

    Should to change Arrays.asList(call).contains(input_num) = false

--- Ans.java
+++ Ans.java
@@ -2,30 +2,30 @@ import java.util.*;
 
 public class Ans {
 
-  // field
-  ArrayList<Integer> ans_num = new ArrayList<Integer>();
-  ArrayList<Integer> ans_list = new ArrayList<Integer>();
-  final int ANS_LENGTH = 3;
+    // field
+    ArrayList<Integer> ans_num = new ArrayList<Integer>();
+    ArrayList<Integer> ans_list = new ArrayList<Integer>();
+    final int ANS_LENGTH = 3;
 
-  // instance
-  Ans(){
-    // -- add list from 1 to 9 and shuffle --
-    for (int i = 0; i < 9; i++){
-      ans_list.add(i+1);
+    // instance
+    Ans(){
+        // -- add list from 1 to 9 and shuffle --
+        for (int i = 0; i < 9; i++){
+            ans_list.add(i+1);
+        }
+        Collections.shuffle(ans_list);
+
+        // -- decide ans number --
+        for (int j = 0; j < ANS_LENGTH; j++) {
+            ans_num.add(ans_list.get(j));
+        }
     }
-    Collections.shuffle(ans_list);
 
-    // -- decide ans number --
-    for (int j = 0; j < ANS_LENGTH; j++) {
-      ans_num.add(ans_list.get(j));
-    }
-  }
+    // method
 
-  // method
-
-  //
-  // public int collation(){
-  //
-  // }
+    //
+    // public int collation(){
+    //
+    // }
 
 }
--- Numer0n.java
+++ Numer0n.java
@@ -2,60 +2,62 @@ import java.util.*;
 
 public class Numer0n {
 
-  public static void main(String[] args) {
-    // Create the instance "ans"
-    Ans ans = new Ans();
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        // Create the instance "ans"
+        Ans ans = new Ans();
+        Scanner scanner = new Scanner(System.in);
 
-    // field
-    // User answer
-    ArrayList<Integer> call = new ArrayList<Integer>(Arrays.asList(0,0,0));
-    int insert_num_to_call = 0;
-    // -- Check to ans_num length --
-    // System.out.println(ans.ans_num.length);
+        // field
+        // User answer
+        ArrayList<Integer> call = new ArrayList<Integer>(Arrays.asList(0,0,0));
+        int insert_num_to_call = 0;
+        // -- Check to ans_num length --
+        // System.out.println(ans.ans_num.length);
 
-    // ** There is for debug **
-    System.out.println("> Answer number is " + ans.ans_num);
+        // ** There is for debug **
+        System.out.println("> Answer number is " + ans.ans_num);
 
 
-    // 入力を促すメッセージ
-    System.out.print("入力してください > ");
-    System.out.println("Your call is " + call);
+        // 入力を促すメッセージ
+        System.out.print("入力してください > ");
+        System.out.println("Your call is " + call);
 
-    while(true){
+        while(true){
 
-      String input_num_str = scanner.nextLine();
-      isNum(input_num_str);
-      int input_num = Integer.parseInt(input_num_str);
+            String input_num_str = scanner.nextLine();
+            if(isNum(input_num_str)){
+                int input_num = Integer.parseInt(input_num_str);
 
-      //入力された内容を画面に表示
-      System.out.println(input_num + "が入力されました");
+                //入力された内容を画面に表示
+                System.out.println(input_num + "が入力されました");
+                // 全部falseが帰ってきてる
+                System.out.println(">> " + Arrays.asList(call).contains(input_num));
+                if (Arrays.asList(call).contains(input_num) || input_num <= 0 || 10 <= input_num){
+                    // Entered a duplicate number
+                    System.out.println("1~9の数字を重複なく入力してください");
+                }else{
+                    call.set(insert_num_to_call, input_num);
+                    insert_num_to_call++;
+                }
+            }
+            System.out.println("Your call is" + call);
 
-      if (Arrays.asList(call).contains(input_num) || input_num <= 0 || 10 <= input_num){
-        // Entered a duplicate number
-        System.out.println("1~9の数字を重複なく入力してください");
-      }else{
-        call.set(insert_num_to_call, input_num);
-        insert_num_to_call++;
-      }
-      System.out.println("Your call is" + call);
+            if(3 <= insert_num_to_call) break;
 
-      if(3 <= insert_num_to_call) break;
+        }
 
+
+        // // Scannerクラスのインスタンスをクローズ
+        // scanner.close();
     }
 
-
-    // // Scannerクラスのインスタンスをクローズ
-    // scanner.close();
-  }
-
-  static boolean isNum(String number) {
-      try {
-          Integer.parseInt(number);
-          return true;
-      } catch (NumberFormatException e) {
-          System.out.println("一桁の数字を入力してください");
-          return false;
-      }
-  }
+    static boolean isNum(String number) {
+        try {
+            Integer.parseInt(number);
+            return true;
+        } catch (NumberFormatException e) {
+            System.out.println("一桁の数字を入力してください");
+            return false;
+        }
+    }
 }
